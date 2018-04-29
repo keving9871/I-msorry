e7d43b5933bd751d0d54ebf4c83b9497699d56c6
Unknown <kah703@nyu.edu>
2018-04-29 19:42:29 -0400

    update

--- Assets/SpriteChanger.cs
+++ Assets/SpriteChanger.cs
@@ -8,6 +8,18 @@ public class SpriteChanger : MonoBehaviour {
     public Sprite BottleH;
     public Sprite Bottle;
 
+    public Sprite Chicken;
+    public Sprite ChickenH;
+
+    public Sprite Steak;
+    public Sprite SteakH;
+
+    public Sprite BB;
+    public Sprite BBH;
+
+    public Sprite BG;
+    public Sprite BGH;
+
     // Use this for initialization
     void Start()
     {
@@ -26,6 +38,50 @@ public class SpriteChanger : MonoBehaviour {
             else
             {
                 this.gameObject.GetComponent<SpriteRenderer>().sprite = Bottle;
+            }
+        }
+        else if (this.gameObject.tag == "Chicken")
+        {
+            if (gameManager.Foods[4].Highlighted == true)
+            {
+                this.gameObject.GetComponent<SpriteRenderer>().sprite = ChickenH;
+            }
+            else
+            {
+                this.gameObject.GetComponent<SpriteRenderer>().sprite = Chicken;
+            }
+        }
+        else if (this.gameObject.tag == "BB")
+        {
+            if (gameManager.Foods[2].Highlighted == true)
+            {
+                this.gameObject.GetComponent<SpriteRenderer>().sprite = BBH;
+            }
+            else
+            {
+                this.gameObject.GetComponent<SpriteRenderer>().sprite = BB;
+            }
+        }
+        else if (this.gameObject.tag == "BG")
+        {
+            if (gameManager.Foods[3].Highlighted == true)
+            {
+                this.gameObject.GetComponent<SpriteRenderer>().sprite = BGH;
+            }
+            else
+            {
+                this.gameObject.GetComponent<SpriteRenderer>().sprite = BG;
+            }
+        }
+        else if (this.gameObject.tag == "Steak")
+        {
+            if (gameManager.Foods[1].Highlighted == true)
+            {
+                this.gameObject.GetComponent<SpriteRenderer>().sprite = SteakH;
+            }
+            else
+            {
+                this.gameObject.GetComponent<SpriteRenderer>().sprite = Steak;
             }
         }
     }
